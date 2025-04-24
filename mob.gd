3078678821d066e6e81f2fa7b2ecd748c9412344
Galy <arthurgaly04@gmail.com>
2025-04-24 19:23:02 +0200

    debug mob by colin

--- mob.gd
+++ mob.gd
@@ -12,34 +12,27 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func _physics_process(delta: float) -> void:
-	
 	if player_chase:
 		position += (player.position - position)/speed
 		if (player.position.x - position.x) < 0:
 			$AnimatedSprite2D.flip_h = true
 		move_and_collide(Vector2(0,0))	
-		
-		
-	
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	player = body# Replace with function body.
-	player_chase = true
-
+	if(body.name=="Player"):
+		player = body# Replace with function body.
+		player_chase = true
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null # Replace with function body.
 	player_chase = false
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.name=="Player"):
 		print("you are dead") # Replace with function body.
 
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
-
 
 func _on_health_manager_death() -> void:
 	emit_signal("death", self)
