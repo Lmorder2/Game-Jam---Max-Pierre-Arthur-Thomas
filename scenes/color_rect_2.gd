e365ac750011c7ee235152ed771dcf296864cb01
Lmorder <thomas.roussel@lecole-ldlc.com>
2025-04-24 08:56:13 +0200

    Séparation des mots en deux scripts parce que c'est mieux. balbutiement des particules

--- scenes/color_rect_2.gd
+++ scenes/color_rect_2.gd
@@ -1,9 +1,9 @@
 extends ColorRect
 
-
+@export var effect_manager : EffectManager
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	show() #Replace hide quand version final
+	hide() #Replace hide quand version final
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("alt"):
 		show()
 	else:
-		show() #replace Hide quand version finale
+		hide() #replace Hide quand version finale
