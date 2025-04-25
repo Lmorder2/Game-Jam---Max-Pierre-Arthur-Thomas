c8a104a2d5a35c329c4695d75fd07ab717f4d812
pierre.chamois <pierre.chamois@lecole-ldlc.com>
2025-04-25 12:22:30 +0200

    effet mot

--- mob.gd
+++ mob.gd
@@ -10,7 +10,6 @@ signal death(mob: Node)
 
 
 var is_stun=false
-signal death(mob: Node)
 var acceleration_ratio = 1.0
 func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("kill_mobs")):
