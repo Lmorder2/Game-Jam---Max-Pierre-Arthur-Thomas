2635263f4a042bfd92a93e169fefa65f4213d28d
Maxime <>
2025-04-25 11:38:52 +0200

    no message

--- chest.gd
+++ chest.gd
@@ -1,25 +1,25 @@
 extends Node2D
 
-@onready var sprite_closed = $closed_chest
-@onready var sprite_opened = $opened_chest
 @export var item_name : String
+
+@onready var animation_player = $AnimatedSprite2D
 
 var is_open = false
 var player_chase = false
 var player = null
 
 func _ready():
-	sprite_closed.visible = true
-	sprite_opened.visible = false
 	$Item/DropChest.hide()
 	
 func _on_body_entered(body):
 	print("Le joueur entre en collision avec le coffre", body.name)
 	if body.name == "Player" and not is_open:
 		is_open = true
-		sprite_closed.visible = false
-		sprite_opened.visible = true
+		animation_player.play("trigger")
 		$Item/DropChest.show()
 
 func _on_grow_body_entered(body: Node2D) -> void:
-	$Item/DropChest.hide()
+	$Item/DropChest.visible = false
+	$Item/dropchest_collision.visible = false
+	Events.item_picked.emit(item_name)
+	print("le joueur reup l'item   " + str(item_name))
