ba14f5321f90cb69a7f1bab76c4455c307c0c967
Maxime <>
2025-04-25 11:46:07 +0200

    chest animation

--- chest.gd
+++ chest.gd
@@ -15,7 +15,7 @@ func _on_body_entered(body):
 	print("Le joueur entre en collision avec le coffre", body.name)
 	if body.name == "Player" and not is_open:
 		is_open = true
-		animation_player.play("trigger")
+		animation_player.play("open_chest")
 		$Item/DropChest.show()
 
 func _on_grow_body_entered(body: Node2D) -> void:
